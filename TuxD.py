4fc4b274ad743e5534f07039924acb9190aec014
Henrik Isefjær Ludvigsen <mail@henrikludvigsen.com>
2026-09-14 01:09:47 +0200

    fixes

--- TuxD.py
+++ TuxD.py
@@ -19,13 +19,13 @@ import ast
 import datetime
 import re
 
-VERSION = "2.0.5"
+VERSION = "2.0.6"
 
 CONFIG_PATH = "config.yaml"
 RELEASES_DIR = "/mnt/storage/tuxd/TuxD/releases"
 LOG_FILE_PATH = "tuxd.log"
 
-UPDATE_MODE = "git"
+UPDATE_MODE = "web"
 
 UPDATE_STATUS_FILE = "update_status.log"
 
