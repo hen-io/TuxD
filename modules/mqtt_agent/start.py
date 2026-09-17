be23fab138133995d900435e6ed5b8c0b98cfd4c
Henrik Isefjær Ludvigsen <mail@henrikludvigsen.com>
2026-09-17 15:56:54 +0200

    fixes

--- modules/mqtt_agent/start.py
+++ modules/mqtt_agent/start.py
@@ -83,7 +83,7 @@ class TuxDAgentMixin(
             "unique_id": f"{self.config['device']['name']}_{object_id}",
             "device": self.device_info,
         }
-        payload["default_entity_id"] = f"sensor.{ha_object_id or object_id}"
+        payload["default_entity_id"] = f"sensor.{ha_object_id or f'{self.device_slug}_{object_id}'}"
         if state_class:
             payload["state_class"] = state_class
         if unit:
@@ -112,7 +112,7 @@ class TuxDAgentMixin(
             "payload_on": "ON",
             "payload_off": "OFF",
         }
-        payload["default_entity_id"] = f"binary_sensor.{object_id}"
+        payload["default_entity_id"] = f"binary_sensor.{self.device_slug}_{object_id}"
         if device_class:
             payload["device_class"] = device_class
         if icon:
@@ -144,7 +144,7 @@ class TuxDAgentMixin(
             payload["icon"] = icon
         if entity_category:
             payload["entity_category"] = entity_category
-        payload["default_entity_id"] = f"update.{ha_object_id or object_id}"
+        payload["default_entity_id"] = f"update.{ha_object_id or f'{self.device_slug}_{object_id}'}"
         if attributes_topic:
             payload["json_attributes_topic"] = attributes_topic
 
@@ -170,7 +170,7 @@ class TuxDAgentMixin(
             payload["icon"] = icon
         if entity_category:
             payload["entity_category"] = entity_category
-        payload["default_entity_id"] = f"text.{ha_object_id or object_id}"
+        payload["default_entity_id"] = f"text.{ha_object_id or f'{self.device_slug}_{object_id}'}"
 
         self.publish(
             self._discovery_topic("text", object_id),
@@ -185,7 +185,7 @@ class TuxDAgentMixin(
             "unique_id": f"{self.config['device']['name']}_{object_id}",
             "device": self.device_info,
         }
-        payload["default_entity_id"] = f"button.{object_id}"
+        payload["default_entity_id"] = f"button.{self.device_slug}_{object_id}"
         if icon:
             payload["icon"] = icon
         if entity_category:
